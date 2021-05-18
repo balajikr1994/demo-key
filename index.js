b6f3b2ccd2eb091e9f03bba359dfdfef4dacdf71
Balaji Ragavan <balaji.r@contus.in>
2021-05-18 19:13:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello Key Auth Check');
+});
+
+app.get('/key-auth', (req, res) => {
+    res.set('Content-Type', 'binary/octet-stream');
+  const fileDIR = path.resolve(`${__dirname}/gudsho.key`);
+  const fileRead = fs.readFileSync(fileDIR);
+  return res.status(200).send(fileRead);
+});
+
+app.listen(3000, () => console.log('Express listening on', 3000));
