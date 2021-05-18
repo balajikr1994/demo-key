ea6f0a8f25dd72d81e987d761d11de347432bb23
Balaji Ragavan <balaji.r@contus.in>
2021-05-18 19:56:11 +0530

    key-auth file

--- index.js
+++ index.js
@@ -11,10 +11,25 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello Key Auth Check');
 });
 
-app.get('/key-auth', (req, res) => {
+/* app.get('/key-auth', (req, res) => {
   const fileDIR = path.resolve(`${__dirname}/gudsho.key`);
   const fileRead = fs.readFileSync(fileDIR);
-  return res.status(200).send(fileRead);
+  return res.status(200).send(fileRead.toString());
+}); */
+
+app.get('/key-auth', function(req, res){
+    var options = {
+        root: path.join(__dirname)
+    };
+      
+    var fileName = 'gudsho.key';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
 });
 
 app.listen(process.env.PORT, () => console.log('Express listening on', process.env.PORT));
