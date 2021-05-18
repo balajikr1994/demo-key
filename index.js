373d5bbdf96db5421fcf9a0534c11bfc90912a75
Balaji Ragavan <balaji.r@contus.in>
2021-05-18 19:45:12 +0530

    cors added

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
+
 const app = express();
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello Key Auth Check');
